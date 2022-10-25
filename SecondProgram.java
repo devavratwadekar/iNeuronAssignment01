b171735fe85108377b6995dcf52245cfe61bc8b9
devavratwadekar <83110314+devavratwadekar@users.noreply.github.com>
2022-10-25 21:24:26 +0530

    Add files via upload

---
+++ SecondProgram.java
@@ -0,0 +1,23 @@
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Print
+		//1111
+		//2222
+		//3333
+		//4444
+		int n=4;
+		for(int i=1;i<=n;i++) 
+		{
+			for(int j=0;j<n;j++) 
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
